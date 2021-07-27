16b7b566046d1ea2b167272f2b97ec6515cd1b84
jakubcze12-GNUplusLinux <85400319+jakubcze12-GNUplusLinux@users.noreply.github.com>
2021-07-27 18:35:30 +0000

    Add files via upload

--- youtube.sh
+++ youtube.sh
@@ -1,20 +1,50 @@
-#!/bin/sh
+#!/bin/bash
 
 #youtube-dl audiomation script 
-#version alpha 0.01
 
+#echo -e -n "Welcome. This program aims to simplify youtube-dl. your youtube-dl version is: "; youtube-dl --version
+#echo "Examples of possible inputs will be shown in square brackets"
+read -p "Enter link (or playlist ID): " link echo 
+read -p "Do you want to download audio or video? [a/v] " av;
+echo -e "These are the contents of you home directory:"
+echo 
+ls ~/; echo 
+read -p "Download destination:(In your ~ home directory)] : " dir
 
-echo ""; echo -n "Welcome to jacob's youtube-dl automation script. your youtube-dl version is: "; youtube-dl --version
-echo ""
-
-read -p "Enter link (or playlist ID): " link
-read -p "Do you want to download audio or video? [a/v]" av
-
+	#In the future, embeding subtitles required
+	#Fix exceptions
 
 	case $av in
 
-		a  ) youtube-dl --embed-thumbnail --extract-audio --audio-format mp3 $link;;
-		v  ) youtube-dl --embed-thumbnail $link;;
+		a)	echo 
+			echo "1) mp3 (default)"
+			echo "2) aac"
+			echo "3) flac"
+			echo "4) m4a"
+			echo 
+			read -p "Choose audio format: [mp3; wav] " aformat
+			echo ""
+
+			youtube-dl --embed-thumbnail --extract-audio --audio-format $aformat -o "~/$dir/%(title)s.%(ext)s" "$link";;
+
+	#IMPORTANT: Make a "default" option for video formats
+
+		v)	echo ""
+			echo "1) mp4 (subtitles supported)"
+			echo "2) flv"
+			echo "3) ogg"
+			echo "4) mkv (subtitles supported)"
+			echo "5) avi"
+			echo "6) Webm (subtitles supported"
+			echo ""
+			read -p "Choose video format: [mp4; flv; ogg] " vformat
+
+
+		       	youtube-dl --recode-video $vformat -o "~/$dir/%(title)s.%(ext)s" --embed-subs "$link";;
+
+		*) 	echo "unexpected input"
+
 
 	esac
 
+exit
