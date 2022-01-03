7a3fc16e19768078425017b81c613c74f7bde794
jakubcze12 <85400319+jakubcze12@users.noreply.github.com>
2022-01-03 21:12:50 +0100

    Add files via upload

--- youtube.sh
+++ youtube.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 downloaddir="$HOME/Music/download" 
-alldir="tmp1 tmp2 tmp3 tmp4 tmp5 tmp6 tmp7 tmp8 tmp9 tmp10"
-#{
+{
 mkdir -p $downloaddir 
-mkdir $downloaddir/$alldir
-#} > /dev/null 2>&1
+mkdir $downloaddir/tmp{1..10}
+} > /dev/null 2>&1
 read -p "Enter link (or playlist ID): " link 
 read -p "Do you want to download audio or video? [a/v] " av;
-ls $HOME/Music/download
+ls -l $HOME/Music/download
 read -p "Download destination: (In /home/jacob/Music/download directory): " dir
 rm -rf $HOME/$downloaddir/$dir
 
@@ -20,13 +19,11 @@ rm -rf $HOME/$downloaddir/$dir
 			echo "4) mp3" 
 			read -p "Choose audio format: [mp3; wav] " aformat
 
-			youtube-dl 
-				
+			youtube-dl \
 				--embed-thumbnail \
 				--extract-audio \
 				--audio-format $aformat \
-				-o "$downloaddir/$dir/%(title)s.%(ext)s" \
-				"$link";;
+				-o "$downloaddir/$dir/%(title)s.%(ext)s" "$link";; 		
 
 		v)	echo ""
 			echo "1) mp4 (subtitles supported)"
